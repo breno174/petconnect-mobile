3fef31522bf8d6b9b86d6fa417a6b99ca965c2f3
breno174 <breno.angelim@hotmail.com>
2025-02-23 12:37:56 -0300

    feat: layout de tela esqueci a senha [#3]

--- petconnect/src/app/(tabs)/_layout.tsx
+++ petconnect/src/app/(tabs)/_layout.tsx
@@ -45,6 +45,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="forgotpassword"
+        options={{
+          title: "Forgot",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="chevron.right" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ petconnect/src/components/ThemedButton.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { ThemedText } from "./ThemedText";
+import { ThemedView } from "./ThemedView";
+
+export type TypeThemedButton = {
+  title: string;
+  type: "blue" | "light";
+};
+
+export function ThemedButton({ title, type = "blue" }: TypeThemedButton) {
+  return (
+    <TouchableOpacity
+      style={[
+        type === "blue" ? styles.default : undefined,
+        type === "light" ? styles.lightButton : undefined,
+      ]}
+    >
+      <ThemedText
+        type="defaultSemiBold"
+        style={[
+          type === "blue"
+            ? { color: styles.default.color, fontSize: styles.default.fontSize }
+            : undefined,
+          type === "light"
+            ? {
+                color: styles.lightButton.color,
+                fontSize: styles.lightButton.fontSize,
+              }
+            : undefined,
+        ]}
+      >
+        {title}
+      </ThemedText>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  default: {
+    borderRadius: 50,
+    width: 220,
+    padding: 10,
+    backgroundColor: "#0496ff",
+    color: "#fff",
+    alignItems: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  lightButton: {
+    borderRadius: 50,
+    width: 220,
+    paddingTop: 10,
+    backgroundColor: "#fff",
+    color: "#0496ff",
+    alignItems: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+});
