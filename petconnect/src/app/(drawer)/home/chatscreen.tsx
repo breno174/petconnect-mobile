395b0c0ef3eacad9ce184f5846c37f66ab662440
breno santos <breno.angelim@hotmail.com>
2025-06-10 00:19:03 -0300

    fix: filter chat

--- petconnect/src/app/(drawer)/home/chatscreen.tsx
+++ petconnect/src/app/(drawer)/home/chatscreen.tsx
@@ -10,6 +10,14 @@ import {
 import { router, useNavigation } from "expo-router";
 import axios from "axios";
 import { AuthUserContext } from "@/src/context/authUserProvider";
+import { createClient } from "@supabase/supabase-js";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
 
 interface User {
   id: number;
@@ -18,26 +26,62 @@ interface User {
 }
 
 export default function UserListScreen() {
+  const variavable = {
+    supabaseurl: process.env.EXPO_PUBLIC_API_SUPABASE_URL,
+    anonkey: process.env.EXPO_PUBLIC_API_SUPABASE_ANON_KEY
+  };
+  if (!variavable.supabaseurl || !variavable.anonkey) {
+    throw new Error("Supabase URL or Anon Key is not defined in environment variables.");
+  }
+  const supabase = createClient(variavable.supabaseurl, variavable.anonkey);
+
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(true);
   const { currentUser } = useContext(AuthUserContext);
+  const [loggedUser, setLoggedUser] = useState<User>();
+
 
   useEffect(() => {
-    currentUser().then((user) => console.log(user));
-
-    axios
-      .get<User[]>("http://localhost:8080/user")
-      .then((response) => {
-        console.log("usuarios", response.data);
-
-        setUsers(response.data);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar usuários:", error);
-      })
-      .finally(() => {
+    currentUser().then((user) => setLoggedUser(user));
+    const fetchUsers = async () => {
+      if (!loggedUser) {
+        console.error("Usuário não está logado");
         setLoading(false);
-      });
+        return;
+      }
+      try {
+        const { data: profiles, error: profilesError } = await supabase
+          .from('messages')
+          .select('*')
+          .eq('sender_id', loggedUser.id)
+          .order('created_at', { ascending: false })
+        console.log(profiles);
+        if (profilesError) {
+          console.error("Erro ao buscar perfis:", profilesError);
+          return;
+        }
+        const receiverIds = profiles.map((profile: any) =>
+          profile.receiver_id
+        );
+        const uniqueReceiverIds = new Set(receiverIds);
+        const allUser = await axios
+          .get<User[]>("http://localhost:8080/user")
+          .then((response) => {
+            console.log("usuarios", response.data);
+            return response.data;
+          })
+          .catch((error) => {
+            console.error("Erro ao buscar usuários:", error);
+            return [];
+          })
+        const filteredUsers = allUser.filter(user => uniqueReceiverIds.has(user.id));
+        setUsers(filteredUsers);
+      } catch (error) {
+        console.error("Erro ao buscar usuários:", error);
+      }
+      setLoading(false);
+    };
+    fetchUsers();
   }, []);
 
   if (loading) {
